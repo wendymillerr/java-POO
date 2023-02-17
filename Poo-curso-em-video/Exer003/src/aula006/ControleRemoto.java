52e0a3605306b413cfbe2ef32f0a189978bb0dce
Wendy Miller <weendymilleer@gmail.com>
2023-02-17 12:04:35 -0300

    alteração final na classe ControleRemoto

--- Poo-curso-em-video/Exer003/src/aula006/ControleRemoto.java
+++ Poo-curso-em-video/Exer003/src/aula006/ControleRemoto.java
@@ -100,7 +100,7 @@ public class ControleRemoto implements Controlador{
   
     public void play() {
         if(this.isLigado() && !this.isTocando()){
-            System.out.println("Play");
+            System.out.println("play sucess");
         }
     }
 }
