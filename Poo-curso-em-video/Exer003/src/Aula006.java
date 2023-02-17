1374fa7dbee4de73bf84c9c8a49f47a99865c4cb
Wendy Miller <weendymilleer@gmail.com>
2023-02-17 12:03:01 -0300

    alteração na main

--- Poo-curso-em-video/Exer003/src/Aula006.java
+++ Poo-curso-em-video/Exer003/src/Aula006.java
@@ -2,8 +2,8 @@ import aula006.ControleRemoto;
 
 public class Aula006 {
     public static void main(String[] args) throws Exception {
-        ControleRemoto controle = new ControleRemoto(50, true, false);
+        ControleRemoto controle = new ControleRemoto(50, true, true);
         controle.abrirMenu();
-        controle.desligarMudo();
+        
     }
 }
