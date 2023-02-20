e832d6a84f37a3147f768f2a59405bc40cba2807
Wendy Miller <weendymilleer@gmail.com>
2023-02-20 19:59:06 -0300

    ultimas alterações

---
+++ Poo-curso-em-video/Exer005/src/Publicaco.java
@@ -0,0 +1,7 @@
+
+public interface Publicaco {
+    public abstract void abrir();
+    public abstract void fechar();
+    public abstract void avancarPag();
+    public abstract void voltarPag();
+}
