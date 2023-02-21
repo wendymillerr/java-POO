86a94470fd579c42c591a9a00ab427a249c6de00
Wendy Miller <weendymilleer@gmail.com>
2023-02-21 11:54:08 -0300

    Pojeto Polimorfismo

---
+++ Poo-curso-em-video/Exer007/src/animal/Peixe.java
@@ -0,0 +1,24 @@
+package animal;
+
+public class Peixe extends Animal {
+    private String corEscama;
+   
+    public void alimentar() {
+       System.out.println("Comendo substâncias");
+    }
+
+   
+    public void emitirSom() {
+      System.out.println("Peixe não emite som");
+    }
+
+
+    public void locomover() {
+        System.out.println("Nadando");
+    }
+
+    public void soltarBolha(){
+        System.out.println("Soltando bolhas...");
+    }
+    
+}
---
+++ Poo-curso-em-video/Exer007/src/animal/Reptil.java
@@ -0,0 +1,15 @@
+package animal;
+
+public class Reptil extends Animal{
+    private String corEscama;
+
+    public void locomover(){
+        System.out.println("Rastejando");
+    }
+    public void alimentar(){
+        System.out.println("Comendo vegetais");
+    }
+    public void emitirSom(){
+        System.out.println("Som de reptil");
+    }
+}
