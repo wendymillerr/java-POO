53f3841a941cd04f601716eb8dcad59664c30f21
Wendy Miller <weendymilleer@gmail.com>
2023-02-21 11:58:24 -0300

    Update polimorfism project

--- Poo-curso-em-video/Exer007/src/App.java
+++ Poo-curso-em-video/Exer007/src/App.java
@@ -1,5 +1,13 @@
+import animal.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Peixe peixe = new Peixe();
+        Reptil lagarto = new Reptil();
+        Mamifero cachorro = new Mamifero();
+
+        peixe.alimentar();
+        lagarto.alimentar();
+
     }
 }
