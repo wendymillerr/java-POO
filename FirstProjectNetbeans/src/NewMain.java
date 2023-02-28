0bfb794e43526c7fce67d3a953311a76df881be0
Wendy Miller <weendymilleer@gmail.com>
2023-02-28 11:13:25 -0300

    first graphic interface using Netbeans

---
+++ FirstProjectNetbeans/src/NewMain.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
