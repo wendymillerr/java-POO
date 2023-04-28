cecb9c69cb642e5d576b1569514cf5e9601a002e
Wendy Miller <weendymilleer@gmail.com>
2023-04-28 18:20:15 -0300

    arquivos deletados]

---
+++ provas/pratica-preprova/src/DataNascimento.java
@@ -0,0 +1,12 @@
+public class DataNascimento {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public DataNascimentoint dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+}
